a5e50c6ab7d04372a4e4153bb1b5bf100bbb80cd
Ariful <arifewucse2016@gmail.com>
2025-03-04 11:13:39 +0600

    create login page

--- lib/views/auth/login_page.dart
+++ lib/views/auth/login_page.dart
@@ -14,6 +14,7 @@ import 'package:foodly/views/auth/widget/password_textfield.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
+//login page
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
